fbd9c59065143f21eaa0b1eedbc2afad6aa04629
lopezbke <kevin.lopez@email.com>
2022-07-31 18:50:39 -0400

    Added inputs to form.

--- src/components/Deploymentform/Deploymentform.js
+++ src/components/Deploymentform/Deploymentform.js
@@ -9,23 +9,55 @@ const printToPdf = () => {
 const Deploymentform = () => {
   return (
     <div className='deploymentForm'>
-      <div className=''>
-        <h1 className='is-size-2'>Deployment Form</h1>
-        <div className='gridFour'>
+     <div className='headingDiv'>
+      <h1 className='is-size-2'>Deployment Form</h1>
+     </div>
+      <div className='box'>
+        <div className='gridTwo'>
           <div>
-            <label>Commits: </label>
-          </div>
-          <div>
+          <label>Commits: </label>
             <textarea className='textarea'></textarea>
           </div>
           <div>
-            <label>Developers: </label>
-          </div>
-          <div>
+          <label>Developers: </label>
             <textarea className='textarea'></textarea>
           </div>
         </div>
-        <button className='button is-primary' onClick={printToPdf}>Print PDF</button>
+        <div>
+          <label>Details/Short Description: </label>
+          <textarea className='textarea'></textarea>
+        </div>
+        <div>
+          <label>Files affected: </label>
+          <textarea className='textarea'></textarea>
+        </div>
+        <div>
+          <label>DataBase: </label>
+          <textarea className='textarea'></textarea>
+        </div>
+        <div>
+          <label>Enviroment: </label>
+            <input className='input' type={'text'}></input>
+          </div>
+        <br></br>
+        <div className='gridTwo'>
+          <div>
+          <label>Deployed By: </label>
+            <input className='input' type={'text'}></input>
+          </div>
+          <div>
+          <label>Deployment Time: </label>
+            <input className='input' type={'datetime-local'}></input>
+          </div>
+        </div>
+        <br></br>
+        <div>
+          <label>Comments/Additional Notes: </label>
+          <textarea className='textarea'></textarea>
+        </div>
+        <div className='printBtnDiv'>
+          <button className='button is-primary' onClick={printToPdf}>Print PDF</button>
+        </div>
       </div>
     </div>
   )
